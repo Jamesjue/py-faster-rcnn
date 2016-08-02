d3eb6c062f9f160465fd5948f92c5a398fade034
Jamesjue <canjian.myself@gmail.com>
2016-08-02 03:07:53 -0400

    moved web_demo to bk. add in customized detect file

---
+++ tools/tpod_detect.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------
+# Faster R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""
+Demo script showing detections in sample images.
+
+See README.md for installation instructions before running.
+"""
+try:
+    import matplotlib as mpl
+    import matplotlib.pyplot as plt
+    mpl.use('Agg')
+except ImportError:
+    print 'no matplotlib. output bounding box in text only'
+import _init_paths
+from fast_rcnn.config import cfg
+from fast_rcnn.test import im_detect
+from fast_rcnn.nms_wrapper import nms
+from utils.timer import Timer
+import numpy as np
+import scipy.io as sio
+import caffe, os, sys, cv2
+import argparse
+import sys
+from textwrap import wrap
+
+CLASSES = ('__background__',
+           'object')
+#           'aeroplane', 'bicycle', 'bird', 'boat',
+#           'bottle', 'bus', 'car', 'cat', 'chair',
+#           'cow', 'diningtable', 'dog', 'horse',
+#           'motorbike', 'person', 'pottedplant',
+#           'sheep', 'sofa', 'train', 'tvmonitor')
+
+NETS = {'vgg16': ('VGG16',
+                  'VGG16_faster_rcnn_final.caffemodel'),
+        'zf': ('ZF',
+                  'ZF_faster_rcnn_final.caffemodel')}
+
+fig, ax = plt.subplots(figsize=(12, 12))
+def vis_detections(im, class_name, dets, thresh=0.5):
+    """Draw detected bounding boxes."""
+    inds = np.where(dets[:, -1] >= thresh)[0]
+    
+    if len(inds) == 0:
+        print 'nothing detected'
+    else:
+        for i in inds:
+            bbox = dets[i, :4]
+            score = dets[i, -1]
+            print 'detected roi:{} score:{}'.format(bbox, score)
+            ax.add_patch(
+                plt.Rectangle((bbox[0], bbox[1]),
+                              bbox[2] - bbox[0],
+                              bbox[3] - bbox[1], fill=False,
+                              edgecolor='red', linewidth=3.5)
+                )
+            ax.text(bbox[0], bbox[1] - 2,
+                    '{:s} {:.3f}'.format(class_name, score),
+                    bbox=dict(facecolor='blue', alpha=0.5),
+                    fontsize=14, color='white')
+
+
+def demo(net, image_name):
+    """Detect object classes in an image using pre-computed object proposals."""
+
+    # Load the demo image
+    im_file = image_name#os.path.join(cfg.DATA_DIR, 'demo', image_name)
+    im = cv2.imread(im_file)
+
+    # Detect all object classes and regress object bounds
+    timer = Timer()
+    timer.tic()
+    scores, boxes = im_detect(net, im)
+    timer.toc()
+    print ('Detection took {:.3f}s for '
+           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
+
+    # Visualize detections for each class
+    im_rgb = im[:, :, (2, 1, 0)]
+    ax.imshow(im_rgb, aspect='equal')
+    
+    CONF_THRESH = 0.75
+    print 'threashold: {}'.format(CONF_THRESH)
+    NMS_THRESH = 0.3
+    for cls_ind, cls in enumerate(CLASSES[1:]):
+        print 'predicting class {}'.format(cls)
+        cls_ind += 1 # because we skipped background
+        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+        cls_scores = scores[:, cls_ind]
+        dets = np.hstack((cls_boxes,
+                          cls_scores[:, np.newaxis])).astype(np.float32)
+        keep = nms(dets, NMS_THRESH)
+        dets = dets[keep, :]
+        print dets
+        if 'matplotlib' in sys.modules:
+            vis_detections(im, cls, dets, thresh=CONF_THRESH)
+
+    ax.set_title(("\n".join(wrap('p({} | box) >= {:.2f}'.format(CLASSES,CONF_THRESH), 40))),
+                  fontsize=14)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.draw()
+            
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Faster R-CNN demo')
+    parser.add_argument('im', help="Input image", default= '000456.jpg')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU device id to use [0]',
+                        default=0, type=int)
+    parser.add_argument('--cpu', dest='cpu_mode',
+                        help='Use CPU mode (overrides --gpu)',
+                        action='store_true')
+    parser.add_argument('--prototxt', dest='prototxt', help='Prototxt of Network')
+    parser.add_argument('--weights', dest='caffemodel', help='Weights of trained network')
+    parser.add_argument('--labels', dest='labels', help='file contain labels',
+                        default=None) 
+    parser.add_argument('--output', dest='destination', help='Output location of image detections')
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+
+    args = parse_args()
+
+    prototxt = args.prototxt#os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
+               #             'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
+    caffemodel = args.caffemodel#os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
+                 #             NETS[args.demo_net][1])
+
+    if not os.path.isfile(caffemodel):
+        raise IOError(('{:s} not found.\nDid you run ./data/script/'
+                       'fetch_faster_rcnn_models.sh?').format(caffemodel))
+
+    if args.cpu_mode:
+        caffe.set_mode_cpu()
+    else:
+        caffe.set_mode_gpu()
+        caffe.set_device(args.gpu_id)
+        cfg.GPU_ID = args.gpu_id
+    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+
+    print '\n\nLoaded network {:s}'.format(caffemodel)
+
+    # j: read in a label file
+    labelfile=args.labels
+    if labelfile and os.path.isfile(labelfile):
+        with open(labelfile, 'r') as f:
+            labels=f.read().splitlines()
+        labels.insert(0, '__background__')
+        CLASSES=(labels)
+    
+    # Warmup on a dummy image
+    # im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
+    # for i in xrange(2):
+    #     _, _= im_detect(net, im)
+
+    demo(net, args.im)
+    #im_names = args.im#['000456.jpg', '000542.jpg', '001150.jpg',
+                #'001763.jpg', '004545.jpg']
+    #for im_name in im_names:
+    #    print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    #    print 'Demo for data/demo/{}'.format(im_name)
+    #    demo(net, im_name)
+
+#    plt.show()
+
+
+    plt.savefig(args.destination)
